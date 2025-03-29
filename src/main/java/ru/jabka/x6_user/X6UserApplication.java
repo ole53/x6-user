94105f8bcd494121931ba499d6849d002e994890
Олег <ole53@yandex.ru>
2025-03-29 23:11:10 +0300

    init

---
+++ src/main/java/ru/jabka/x6_user/X6UserApplication.java
@@ -0,0 +1,13 @@
+package ru.jabka.x6_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X6UserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jabka/x6_user/X6UserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jabka.x6_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
